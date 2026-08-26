8c02c44708098cc58ebfabde1a101d925c6f9cc2
Zytka <zyta.slowianska@gmail.com>
2026-08-26 14:48:25 +0200

    feat: Completed the simple version of guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::io;
 
 use rand::Rng;
@@ -5,19 +6,31 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is {secret_number}");
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess) // &mut guess - reference,
-                               // which gives you a way to let multiple parts of your code
-                               // access one piece of data without needing to copy
-                               // that data into memory multiple times
-                               // just like variables references are immutable by default
-        .expect("Failed to read line"); // We put an expectation on the `read_line()` function's `Result``
-    println!("You guessed: {guess}")
+        io::stdin()
+            .read_line(&mut guess) // &mut guess - reference,
+                                // which gives you a way to let multiple parts of your code
+                                // access one piece of data without needing to copy
+                                // that data into memory multiple times
+                                // just like variables references are immutable by default
+            .expect("Failed to read line"); // We put an expectation on the `read_line()` function's `Result``
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
